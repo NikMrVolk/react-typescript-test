41d2e5d752355ba6185d4cac8db2615f6d610baf
NickZavadski <nick.mr.volk@gmail.com>
2023-11-06 19:19:42 +0300

    work with props

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Card, { CardVariant } from './components/Card'
+
 const App = () => {
-	return <div>Work</div>
+	return (
+		<div>
+			<Card width="200px" height="200px" variant={CardVariant.outlined} onClick={(num) => console.log('click', num)}>
+				<button>123</button>
+			</Card>
+		</div>
+	)
 }
 
 export default App
