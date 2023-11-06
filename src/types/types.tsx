81bbbe363c8e9a51d79df32667b10635858a0edc
NickZavadski <nick.mr.volk@gmail.com>
2023-11-06 21:56:57 +0300

    change interface to types

--- src/types/types.tsx
+++ src/types/types.tsx
@@ -1,18 +1,20 @@
+export interface IWithId {
+	id: number
+}
+
 export interface IAddress {
 	street: string
 	city: string
 	zipcode: string
 }
 
-export interface IUser {
-	id: number
+export type IUser = IWithId & {
 	name: string
 	email: string
 	address: IAddress
 }
 
-export interface ITodo {
-	id: number
+export type ITodo = IWithId & {
 	userId: number
 	title: string
 	completed: boolean
